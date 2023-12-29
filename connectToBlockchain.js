ccdd5b5f48057beccc6021c3e4c4e499c76aa2e6
meda2001 <medamurariprasad2001@gmail.com>
2023-12-29 11:52:23 +0530

    fetched accounts

--- connectToBlockchain.js
+++ connectToBlockchain.js
@@ -1,8 +1,14 @@
 const {Web3} = require('web3');
 
 async function connect(){
-    const myProvider= new Web3("HTTP://127.0.0.1:7545");
-    console.log(myProvider);
+    const myProvider= new Web3("HTTP://127.0.0.1:7545");  // connected to blockchain world
+    // console.log(myProvider);
+    const web3 =new Web3(myProvider);  // for checkig purpsoe
+    // console.log(web3);
+
+    const accounts = await web3.eth.getAccounts(); // fetch list of accounts
+    console.log(accounts);
+    console.log(accounts[0]);
 
 
 }
